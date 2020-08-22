36212dcdbd6a385e2d0ad013e3e0e4b14ef32a5d
nagase123 <nagase0926@gmail.com>
2020-08-22 21:26:21 +0900

    コメント機能追加

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <v-app>
     <v-container>
       <ChatBoard />
+      <ChatForm />
     </v-container>
   </v-app>
 </template>
 
 <script>
 import ChatBoard from "./components/ChatBoard";
+import ChatForm from "./components/Form";
 
 export default {
   name: "App",
   components: {
     ChatBoard,
+    ChatForm,
   },
 };
 </script>
